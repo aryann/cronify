6d9be9d159e6c8a8ef09c603e9119e450d21fd2b
Aryan Naraghi <aryan.naraghi@gmail.com>
2020-06-30 16:56:27 -0700

    Adds documentation.

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
+import os
 import setuptools
+
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                       'README.md')) as f:
+    long_description = f.read()
+
 
 setuptools.setup(
     name='schedulify',
     version='0.0.1',
     author='Aryan Naraghi',
     author_email='aryan.naraghi@gmail.com',
+    description=('Schedule Python functions on Google Cloud Platform '
+                 'with minimal boilerplate'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=[
         'schedulify',
     ],
