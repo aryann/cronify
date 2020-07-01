0104e5bcec8094f9543e1ffb00c2e1d37e26ecec
Aryan Naraghi <aryan.naraghi@gmail.com>
2020-06-30 17:12:54 -0700

    Adds a license + additional metadata.

--- setup.py
+++ setup.py
@@ -9,13 +9,27 @@ with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
 
 setuptools.setup(
     name='schedulify',
-    version='0.0.1',
+    version='0.0.2',
     author='Aryan Naraghi',
     author_email='aryan.naraghi@gmail.com',
     description=('Schedule Python functions on Google Cloud Platform '
                  'with minimal boilerplate'),
     long_description=long_description,
     long_description_content_type='text/markdown',
+    url='https://github.com/aryann/schedulify',
+    project_urls={
+        'Source Code': 'https://github.com/aryann/schedulify',
+        "Author's Website": 'https://aryan.app',
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+    ],
     packages=[
         'schedulify',
     ],
